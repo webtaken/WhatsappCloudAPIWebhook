b137b34dd2779ad46935ee10ec2fa3f3250592bf
Saul Rojas <luckly083@gmail.com>
2022-08-05 15:55:40 -0500

    corregido el nombre del emisor corregido

--- FlowFunctions/DataExtractor.js
+++ FlowFunctions/DataExtractor.js
@@ -8,7 +8,7 @@ const DataExtractor = (wpp_object) => {
 
       let my_phone_number = wpp_object.entry[0].changes[0].value.metadata.phone_number_id;
       let sender_phone_number = wpp_object.entry[0].changes[0].value.messages[0].from;
-      let sender_name = wpp_object.entry[0].changes[0].value.contacts;
+      let sender_name = wpp_object.entry[0].changes[0].value.contacts[0].profile.name;
       let message = wpp_object.entry[0].changes[0].value.messages[0];
 
       return {
