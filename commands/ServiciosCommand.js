f17bcb4a409ea774d213773845aca02302de2056
Saul Rojas <luckly083@gmail.com>
2022-08-07 10:29:50 -0500

    haciendo una prueba al mensaje de los servicios

--- commands/ServiciosCommand.js
+++ commands/ServiciosCommand.js
@@ -22,7 +22,7 @@ let services_metadata = {
 
 const ServiciosCommand = (my_phone_number, username, to_phone_number) => {
 
-  const servicesList = {
+  const anotherServicesList = {
     messaging_product: "whatsapp",
     recipient_type: "individual",
     to: to_phone_number,
@@ -68,6 +68,61 @@ const ServiciosCommand = (my_phone_number, username, to_phone_number) => {
       }
     }
   };
+
+  const servicesList = {
+    "messaging_product": "whatsapp",
+    "recipient_type": "individual",
+    "to": to_phone_number,
+    "type": "interactive",
+    "interactive": {
+      "type": "list",
+      "header": {
+        "type": "text",
+        "text": "<HEADER_TEXT>"
+      },
+      "body": {
+        "text": "<BODY_TEXT>"
+      },
+      "footer": {
+        "text": "<FOOTER_TEXT>"
+      },
+      "action": {
+        "button": "<BUTTON_TEXT>",
+        "sections": [
+          {
+            "title": "<LIST_SECTION_1_TITLE>",
+            "rows": [
+              {
+                "id": "<LIST_SECTION_1_ROW_1_ID>",
+                "title": "<SECTION_1_ROW_1_TITLE>",
+                "description": "<SECTION_1_ROW_1_DESC>"
+              },
+              {
+                "id": "<LIST_SECTION_1_ROW_2_ID>",
+                "title": "<SECTION_1_ROW_2_TITLE>",
+                "description": "<SECTION_1_ROW_2_DESC>"
+              }
+            ]
+          },
+          {
+            "title": "<LIST_SECTION_2_TITLE>",
+            "rows": [
+              {
+                "id": "<LIST_SECTION_2_ROW_1_ID>",
+                "title": "<SECTION_2_ROW_1_TITLE>",
+                "description": "<SECTION_2_ROW_1_DESC>"
+              },
+              {
+                "id": "<LIST_SECTION_2_ROW_2_ID>",
+                "title": "<SECTION_2_ROW_2_TITLE>",
+                "description": "<SECTION_2_ROW_2_DESC>"
+              }
+            ]
+          }
+        ]
+      }
+    }
+  }
 
   sendWppRequest({
     method: 'POST',
