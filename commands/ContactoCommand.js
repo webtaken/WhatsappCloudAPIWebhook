e8a128d73313171e3e01a43237fac66da75f6c14
Saul Rojas <luckly083@gmail.com>
2022-08-08 14:07:51 -0500

    imagen de contacto cambiada

--- commands/ContactoCommand.js
+++ commands/ContactoCommand.js
@@ -2,7 +2,7 @@ const SendImageMessage = require('../EssentialFunctions/SendImageMessage');
 
 const ContactoCommand = (my_phone_number, username, to_phone_number) => {
   const contactoImageConf = {
-    link: "https://i.imgur.com/uABTOC2.png",
+    link: "https://i.imgur.com/QIVQYEZ.jpg",
     caption: `${username} puedes contactarnos 💬 por los siguientes medios: +51969601133, +51933254667 (recomendado *Whatsapp business*) o telemark.inc.pe@gmail.com`
   };
   SendImageMessage(my_phone_number, to_phone_number, contactoImageConf);
