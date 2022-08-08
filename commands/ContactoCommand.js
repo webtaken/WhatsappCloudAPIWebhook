2cc715d86440c1dc6f3b62db15c231ff6ba3d2f8
Saul Rojas <luckly083@gmail.com>
2022-08-08 14:10:07 -0500

    los datos de contacto fueron cambiados

--- commands/ContactoCommand.js
+++ commands/ContactoCommand.js
@@ -3,7 +3,7 @@ const SendImageMessage = require('../EssentialFunctions/SendImageMessage');
 const ContactoCommand = (my_phone_number, username, to_phone_number) => {
   const contactoImageConf = {
     link: "https://i.imgur.com/QIVQYEZ.jpg",
-    caption: `${username} puedes contactarnos 💬 por los siguientes medios: +51969601133, +51933254667 (recomendado *Whatsapp business*) o telemark.inc.pe@gmail.com`
+    caption: `${username} puedes contactarnos 💬 por los siguientes medios: +51969601133, +51933254667 (recomendado *Whatsapp business*), telemark.inc.pe@gmail.com, o a nuestra página web: telemarkinc-2515b.web.app`
   };
   SendImageMessage(my_phone_number, to_phone_number, contactoImageConf);
 };
